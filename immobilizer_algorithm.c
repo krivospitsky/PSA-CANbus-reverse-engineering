acc3e54560ac96324ae0f39b652a0c50792245b9
prototux <jason@prototux.net>
2020-03-25 23:26:19 +0100

    update immobilizer

--- immobilizer_algorithm.c
+++ immobilizer_algorithm.c
@@ -6,29 +6,25 @@
 #include <inttypes.h>
 
 // Transformation function with PSA not-so-secret sauce
-int16_t transform(int16_t i, int8_t sec[])
+int16_t transform(int16_t data_msb, int16_t data_lsb, int8_t sec[])
 {
-	int16_t result = ((i % sec[0]) * sec[2]) - ((i / sec[0]) * sec[1]);
+	int16_t data = (data_msb << 16) | data_lsb;
+	int16_t result = ((data % sec[0]) * sec[2]) - ((data / sec[0]) * sec[1]);
 	if (result < 0)
 		result += (sec[0] * sec[2]) + sec[1];
 	return result;
 }
 
 // Challenge reponse calculation for a given pin and challenge
-uint32_t compute_response(uint32_t pin, uint32_t challenge)
+// Challenge and pin are both 4*8bits values
+uint32_t compute_response(uint8_t pin[], uint8_t chg[])
 {
 	// Hardcoded secrets...
 	int8_t sec_1[3] = {0xB2, 0x3F, 0xAA};
 	int8_t sec_2[3] = {0xB1, 0x02, 0xAB};
 
-	// Compute first sub_challenge parts and sub_pin parts
-	int16_t sub_ch_1 = ((challenge & 0xff000000) >> 16) | ((challenge & 0x0000ff00) >> 8);
-	int16_t sub_ch_2 = ((challenge & 0x00ff0000) >> 8) | (challenge & 0x000000ff);
-	int16_t sub_pin_1 = ((pin & 0xff000000) >> 16) | (pin & 0x000000ff);
-	int16_t sub_pin_2 = (pin & 0x00ffff00) >> 8;
-
 	// Compute each 16bits part of the response and return it
-	int16_t res_l = transform(sub_ch_1, sec_1) | transform(sub_pin_1, sec_2);
-	int16_t res_r = transform(sub_ch_2, sec_2) | transform(sub_pin_2, sec_1);
-	return (res_l << 16) | res_r;
+	int16_t res_msb = transform(chg[0], chg[2], sec_1) | transform(pin[0], pin[3], sec_2);
+	int16_t res_lsb = transform(chg[1], chg[3], sec_2) | transform(pin[1], pin[2], sec_1);
+	return (res_msb << 16) | res_lsb;
 }
